fcbf941e237a51f1f40faa4e4c4d60fbc566f7f1
Masha Kokhanchik <masha.kokhanchik@gmail.com>
2022-05-14 12:35:05 +0300

    Create project

---
+++ SwiftUI_VKClient/SwiftUI_VKClient/SwiftUI_VKClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VKClientApp.swift
+//  SwiftUI_VKClient
+//
+//  Created by Мария Коханчик on 14.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VKClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
